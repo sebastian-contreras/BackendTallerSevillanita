40b3ab6f4d851cb082538a89c36efee39e0b0514
Sebastian Contreras <sebasfac2017@gmail.com>
2024-03-05 12:02:29 -0300

    Se implementa Controller Partes

--- src/main/java/com/SebastianContreras/SoftTaller/persistence/ParteDao.java
+++ src/main/java/com/SebastianContreras/SoftTaller/persistence/ParteDao.java
@@ -1,4 +1,42 @@
 package com.SebastianContreras.SoftTaller.persistence;
 
+import com.SebastianContreras.SoftTaller.entities.Parte;
+import com.SebastianContreras.SoftTaller.repositories.ParteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
 public class ParteDao {
+    @Autowired
+    ParteRepository repository;
+
+    public List<Parte> findAll() {
+        return repository.findAll();
+    }
+
+    public Page<Parte> findAllPage(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+
+    public Optional<Parte> findById(String id) {
+        return repository.findById(id);
+    }
+
+    public Parte save(Parte parte) {
+        return repository.saveAndFlush(parte);
+    }
+
+    public Parte update(Parte parte) {
+        return repository.save(parte);
+    }
+
+    public void deleteById(String id) {
+        repository.deleteById(id);
+    }
 }
--- src/main/java/com/SebastianContreras/SoftTaller/services/ParteService.java
+++ src/main/java/com/SebastianContreras/SoftTaller/services/ParteService.java
@@ -1,4 +1,43 @@
 package com.SebastianContreras.SoftTaller.services;
 
+import com.SebastianContreras.SoftTaller.entities.Parte;
+import com.SebastianContreras.SoftTaller.persistence.ParteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ParteService {
+    @Autowired
+    private ParteDao parteDao;
+
+    public List<Parte> findAll() {
+        return parteDao.findAll();
+    }
+
+    public Page<Parte> findAllPage(Pageable pageable) {
+        return parteDao.findAllPage(pageable);
+    }
+
+    public Optional<Parte> findById(String id) {
+        return parteDao.findById(id);
+    }
+
+
+    public Parte save(Parte parte) {
+        return parteDao.save(parte);
+    }
+
+    public Parte update(Parte parte) {
+        return parteDao.update(parte);
+    }
+
+
+    public void deleteById(String id) {
+        parteDao.deleteById(id);
+    }
 }
